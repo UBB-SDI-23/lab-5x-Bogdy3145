6eee956871c85f5e5b97f5f30f47312dbd1462f7
Bogdy3145 <71905083+Bogdy3145@users.noreply.github.com>
2023-04-03 00:06:59 +0300

    aaa nu merge statistica

--- react-frontend-1/src/App.tsx
+++ react-frontend-1/src/App.tsx
@@ -7,6 +7,8 @@ import { BrandAdd } from "./components/CarBrand/BrandAdd";
 import { BrandDelete } from "./components/CarBrand/BrandDelete";
 import { BrandDetails } from "./components/CarBrand/BrandDetails";
 import { BrandUpdate } from "./components/CarBrand/BrandUpdate";
+import { AverageProdYear } from "./components/statistics/AverageProdYear";
+
 ///
 ///
 function App() {
@@ -18,12 +20,13 @@ function App() {
 				<Routes>
 					<Route path="/" element={<AppHome />} />
 					<Route path="/brands" element={<AllBrands />} />
-				  <Route path="/brands/add" element={<BrandAdd />} />
-          
+					<Route path="/brands/add" element={<BrandAdd />} />
+			
 					<Route path="brands/:brandId/delete" element={<BrandDelete />} />
-          <Route path="brands/:brandId/details" element={<BrandDetails />} />
-          <Route path="/brands/:brandId/edit" element={<BrandUpdate />} />
-          
+					<Route path="brands/:brandId/details" element={<BrandDetails />} />
+					<Route path="/brands/:brandId/edit" element={<BrandUpdate />} />
+
+					<Route path="/averageprodyear" element={<AverageProdYear/>}></Route>
           {/*
           <Route path="/owners/:ownerId/edit" element={<OwnerUpdate />} />
 					<Route path="/owners/:ownerId/delete" element={<OwnerDelete />} />
